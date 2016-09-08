317bc3849e21dc3a6acfa6d2cfa76e39433d75fa
Eddie Kohler <ekohler@gmail.com>
2016-09-08 17:09:59 -0400

    Add mb-cheat so you can see how it works.

---
+++ fundamentals3x/mb-cheat.c
@@ -0,0 +1,22 @@
+#include "membench.h"
+#include <stdlib.h>
+#include <assert.h>
+
+static chunk the_only_chunk;
+
+membench_arena* membench_arena_new(void) {
+    return NULL;
+}
+
+chunk* membench_alloc(membench_arena* arena) {
+    (void) arena;
+    return &the_only_chunk;
+}
+
+void membench_free(membench_arena* arena, chunk* x) {
+    (void) arena, (void) x;
+}
+
+void membench_arena_free(membench_arena* arena) {
+    (void) arena;
+}
