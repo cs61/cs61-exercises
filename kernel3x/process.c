788fe41edffbf7004d0ec6f845b917388ffc9aee
Eddie Kohler <ekohler@gmail.com>
2016-11-01 09:17:31 -0700

    Fix user-level panic.

--- kernel3x/process.c
+++ kernel3x/process.c
@@ -29,20 +29,14 @@ void panic(const char* format, ...) {
     va_list val;
     va_start(val, format);
     char buf[160];
-    memcpy(buf, "PANIC: ", 7);
-    int len = vsnprintf(&buf[7], sizeof(buf) - 7, format, val) + 7;
+    int len = vsnprintf(buf, sizeof(buf), format, val);
     va_end(val);
     if (len > 0 && buf[len - 1] != '\n')
         strcpy(buf + len - (len == (int) sizeof(buf) - 1), "\n");
-    (void) console_printf(CPOS(23, 0), 0xC000, "%s", buf);
-    sys_panic(NULL);
+    sys_panic(buf);
  spinloop: goto spinloop;       // should never get here
 }
 
 void assert_fail(const char* file, int line, const char* msg) {
-    (void) console_printf(CPOS(23, 0), 0xC000,
-                          "PANIC: %s:%d: assertion '%s' failed\n",
-                          file, line, msg);
-    sys_panic(NULL);
- spinloop: goto spinloop;       // should never get here
+    panic("%s:%d: assertion '%s' failed\n", file, line, msg);
 }
