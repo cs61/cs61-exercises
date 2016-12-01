8d408100512f93b55ab9de7503350068762e86f9
Eddie Kohler <ekohler@gmail.com>
2016-12-01 13:32:31 -0500

    Synch4 programs

---
+++ synch4/incr.c
@@ -0,0 +1,27 @@
+//! -O0
+#include <pthread.h>
+#include <stdio.h>
+
+void* threadfunc(void* arg) {
+    unsigned* x = (unsigned*) arg;
+    for (int i = 0; i != 10000000; ++i)
+        ++*x;
+    return 0;
+}
+
+void* atomic_threadfunc(void* arg) {
+    unsigned* x = (unsigned*) arg;
+    for (int i = 0; i != 10000000; ++i)
+        __sync_fetch_and_add(x, 1);
+    return 0;
+}
+
+int main() {
+    pthread_t th[4];
+    unsigned n = 0;
+    for (int i = 0; i != 4; ++i)
+        pthread_create(&th[i], NULL, atomic_threadfunc, (void*) &n);
+    for (int i = 0; i != 4; ++i)
+        pthread_join(th[i], NULL);
+    printf("%u\n", n);
+}
