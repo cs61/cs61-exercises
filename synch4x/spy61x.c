596966ba522404a3bca25b54e82e3387994ae393
Eddie Kohler <ekohler@gmail.com>
2016-12-01 15:41:49 -0500

    SPY

---
+++ synch4x/spy61x.c
@@ -0,0 +1,340 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
+#include <assert.h>
+#include <pthread.h>
+
+static const char* spy_host = "cs61.seas.harvard.edu";
+static const char* spy_port = "6164";
+static const char* spy_user = "virginiahall";
+static struct addrinfo* spy_addr;
+
+
+// TIME HELPERS
+double elapsed_base = 0;
+
+// timestamp()
+//    Return the current absolute time as a real number of seconds.
+double timestamp(void) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    return now.tv_sec + (double) now.tv_usec / 1000000;
+}
+
+// elapsed()
+//    Return the number of seconds that have elapsed since `elapsed_base`.
+double elapsed(void) {
+    return timestamp() - elapsed_base;
+}
+
+
+// HTTP CONNECTION MANAGEMENT
+
+// http_connection
+//    This object represents an open HTTP connection to a server.
+typedef struct http_connection http_connection;
+struct http_connection {
+    int fd;                 // Socket file descriptor
+
+    int state;              // Response parsing status (see below)
+    int status_code;        // Response status code (e.g., 200, 402)
+    size_t content_length;  // Content-Length value
+    int has_content_length; // 1 iff Content-Length was provided
+    int eof;                // 1 iff connection EOF has been reached
+
+    char buf[BUFSIZ];       // Response buffer
+    size_t len;             // Length of response buffer
+};
+
+// `http_connection::state` constants
+#define HTTP_REQUEST 0      // Request not sent yet
+#define HTTP_INITIAL 1      // Before first line of response
+#define HTTP_HEADERS 2      // After first line of response, in headers
+#define HTTP_BODY    3      // In body
+#define HTTP_DONE    (-1)   // Body complete, available for a new request
+#define HTTP_CLOSED  (-2)   // Body complete, connection closed
+#define HTTP_BROKEN  (-3)   // Parse error
+
+// helper functions
+char* http_truncate_response(http_connection* conn);
+static int http_process_response_headers(http_connection* conn);
+static int http_check_response_body(http_connection* conn);
+
+static void usage(void);
+
+
+// http_connect(ai)
+//    Open a new connection to the server described by `ai`. Returns a new
+//    `http_connection` object for that server connection. Exits with an
+//    error message if the connection fails.
+http_connection* http_connect(const struct addrinfo* ai) {
+    // connect to the server
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    int yes = 1;
+    (void) setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+    int r = connect(fd, ai->ai_addr, ai->ai_addrlen);
+    if (r < 0) {
+        perror("connect");
+        exit(1);
+    }
+
+    // construct an http_connection object for this connection
+    http_connection* conn =
+        (http_connection*) malloc(sizeof(http_connection));
+    conn->fd = fd;
+    conn->state = HTTP_REQUEST;
+    conn->eof = 0;
+    return conn;
+}
+
+
+// http_close(conn)
+//    Close the HTTP connection `conn` and free its resources.
+void http_close(http_connection* conn) {
+    close(conn->fd);
+    free(conn);
+}
+
+
+// http_send_request(conn, uri)
+//    Send an HTTP POST request for `uri` to connection `conn`.
+//    Exit on error.
+void http_send_request(http_connection* conn, const char* uri) {
+    assert(conn->state == HTTP_REQUEST || conn->state == HTTP_DONE);
+
+    // prepare and write the request
+    char reqbuf[BUFSIZ];
+    size_t reqsz = snprintf(reqbuf, sizeof(reqbuf),
+                            "POST /%s/%s HTTP/1.0\r\n"
+                            "Host: %s\r\n"
+                            "Connection: keep-alive\r\n"
+                            "\r\n",
+                            spy_user, uri, spy_host);
+    assert(reqsz < sizeof(reqbuf));
+
+    size_t pos = 0;
+    while (pos < reqsz) {
+        ssize_t nw = write(conn->fd, &reqbuf[pos], reqsz - pos);
+        if (nw == 0)
+            break;
+        else if (nw == -1 && errno != EINTR && errno != EAGAIN) {
+            perror("write");
+            exit(1);
+        } else if (nw != -1)
+            pos += nw;
+    }
+
+    if (pos != reqsz) {
+        fprintf(stderr, "%.3f sec: connection closed prematurely\n",
+                elapsed());
+        exit(1);
+    }
+
+    // clear response information
+    conn->state = HTTP_INITIAL;
+    conn->status_code = -1;
+    conn->content_length = 0;
+    conn->has_content_length = 0;
+    conn->len = 0;
+}
+
+
+// http_receive_response_headers(conn)
+//    Read the server's response headers. On return, `conn->status_code`
+//    holds the server's status code. If the connection terminates
+//    prematurely, `conn->status_code` is -1.
+void http_receive_response_headers(http_connection* conn) {
+    assert(conn->state != HTTP_REQUEST);
+    if (conn->state < 0)
+        return;
+
+    // read & parse data until told `http_process_response_headers`
+    // tells us to stop
+    while (http_process_response_headers(conn)) {
+        ssize_t nr = read(conn->fd, &conn->buf[conn->len], BUFSIZ);
+        if (nr == 0)
+            conn->eof = 1;
+        else if (nr == -1 && errno != EINTR && errno != EAGAIN) {
+            perror("read");
+            exit(1);
+        } else if (nr != -1)
+            conn->len += nr;
+    }
+
+    // Status codes >= 500 mean we are overloading the server
+    // and should exit.
+    if (conn->status_code >= 500) {
+        fprintf(stderr, "%.3f sec: exiting because of "
+                "server status %d (%s)\n", elapsed(),
+                conn->status_code, http_truncate_response(conn));
+        exit(1);
+    }
+}
+
+
+// http_receive_response_body(conn)
+//    Read the server's response body. On return, `conn->buf` holds the
+//    response body, which is `conn->len` bytes long and has been
+//    null-terminated.
+void http_receive_response_body(http_connection* conn) {
+    assert(conn->state < 0 || conn->state == HTTP_BODY);
+    if (conn->state < 0)
+        return;
+
+    // read response body (http_check_response_body tells us when to stop)
+    while (http_check_response_body(conn)) {
+        ssize_t nr = read(conn->fd, &conn->buf[conn->len], BUFSIZ);
+        if (nr == 0)
+            conn->eof = 1;
+        else if (nr == -1 && errno != EINTR && errno != EAGAIN) {
+            perror("read");
+            exit(1);
+        } else if (nr != -1)
+            conn->len += nr;
+    }
+
+    // null-terminate body
+    conn->buf[conn->len] = 0;
+}
+
+
+// http_truncate_response(conn)
+//    Truncate the `conn` response text to a manageable length and return
+//    that truncated text. Useful for error messages.
+char* http_truncate_response(http_connection* conn) {
+    char *eol = strchr(conn->buf, '\n');
+    if (eol)
+        *eol = 0;
+    if (strnlen(conn->buf, 100) >= 100)
+        conn->buf[100] = 0;
+    return conn->buf;
+}
+
+
+// MAIN PROGRAM
+
+// usage()
+//    Explain how spy61 should be run.
+static void usage(void) {
+    fprintf(stderr, "Usage: ./spy61 [-h HOST] [-p PORT] [USER]\n");
+    exit(1);
+}
+
+
+// main(argc, argv)
+//    The main loop.
+int main(int argc, char** argv) {
+    // parse arguments
+    int ch, nocheck = 0;
+    while ((ch = getopt(argc, argv, "nh:p:u:")) != -1) {
+        if (ch == 'h')
+            spy_host = optarg;
+        else if (ch == 'p')
+            spy_port = optarg;
+        else if (ch == 'u')
+            spy_user = optarg;
+        else if (ch == 'n')
+            nocheck = 1;
+        else
+            usage();
+    }
+    if (optind == argc - 1)
+        spy_user = argv[optind];
+    else if (optind != argc)
+        usage();
+
+    // look up network address of spy server
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_NUMERICSERV;
+    int r = getaddrinfo(spy_host, spy_port, &hints, &spy_addr);
+    if (r != 0) {
+        fprintf(stderr, "problem looking up %s: %s\n",
+                spy_host, gai_strerror(r));
+        exit(1);
+    }
+
+    // measure future times relative to this moment
+    elapsed_base = timestamp();
+
+    // spy
+    http_connection* conn = http_connect(spy_addr);
+    while (1) {
+        // YOUR CODE HERE!
+        http_send_request(conn, "mission");
+        http_receive_response_headers(conn);
+        if (conn->status_code != 200)
+            exit(1);
+        http_receive_response_body(conn);
+        fprintf(stderr, "MISSION: %s\n", conn->buf);
+    }
+}
+
+
+// HTTP PARSING
+
+// http_process_response_headers(conn)
+//    Parse the response represented by `conn->buf`. Returns 1
+//    if more header data remains to be read, 0 if all headers
+//    have been consumed.
+static int http_process_response_headers(http_connection* conn) {
+    size_t i = 0;
+    while ((conn->state == HTTP_INITIAL || conn->state == HTTP_HEADERS)
+           && i + 2 <= conn->len) {
+        if (conn->buf[i] == '\r' && conn->buf[i+1] == '\n') {
+            conn->buf[i] = 0;
+            if (conn->state == HTTP_INITIAL) {
+                int minor;
+                if (sscanf(conn->buf, "HTTP/1.%d %d",
+                           &minor, &conn->status_code) == 2)
+                    conn->state = HTTP_HEADERS;
+                else
+                    conn->state = HTTP_BROKEN;
+            } else if (i == 0)
+                conn->state = HTTP_BODY;
+            else if (strncmp(conn->buf, "Content-Length: ", 16) == 0) {
+                conn->content_length = strtoul(conn->buf + 16, NULL, 0);
+                conn->has_content_length = 1;
+            }
+            memmove(conn->buf, conn->buf + i + 2, conn->len - (i + 2));
+            conn->len -= i + 2;
+            i = 0;
+        } else
+            ++i;
+    }
+
+    if (conn->eof)
+        conn->state = HTTP_BROKEN;
+    return conn->state == HTTP_INITIAL || conn->state == HTTP_HEADERS;
+}
+
+
+// http_check_response_body(conn)
+//    Returns 1 if more response data should be read into `conn->buf`,
+//    0 if the connection is broken or the response is complete.
+static int http_check_response_body(http_connection* conn) {
+    if (conn->state == HTTP_BODY
+        && (conn->has_content_length || conn->eof)
+        && conn->len >= conn->content_length)
+        conn->state = HTTP_DONE;
+    if (conn->eof && conn->state == HTTP_DONE)
+        conn->state = HTTP_CLOSED;
+    else if (conn->eof)
+        conn->state = HTTP_BROKEN;
+    return conn->state == HTTP_BODY;
+}
